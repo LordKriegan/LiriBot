8e62796dbe9f00847febe59f67a13b2f905ceb7d
Qamar Stationwala <YoPappy0@gmail.com>
2017-11-20 03:29:35 -0800

    initial commit, still need to install dependencies

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'j52vJ99VUyFL5O3owxFkVPsp8',
+  consumer_secret: '9TDSi1nJK6mrIMAdE75vsqSxcS77t0Zt2frVsDcw6tFugDS9SJ',
+  access_token_key: '932566318007533569-LBErrkuzkHgOp5K1Cy1SNyThhshuIWS',
+  access_token_secret: 'ULTBPXCPJbJJ7DlPWqxqzCIC6z2LjpGlMbmzMP0nME7TD',
+}
+
+module.exports = twitterKeys;
