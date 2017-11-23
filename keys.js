dbbce5fcc57a3149d7674f4a67dcb1dac244c48b
Qamar Stationwala <YoPappy0@gmail.com>
2017-11-23 08:58:34 -0800

    spotify-this-song now works, also searches The Sign by Ace of Base if no song is found

--- keys.js
+++ keys.js
@@ -1,8 +1,17 @@
-var twitterKeys = {
-  consumer_key: 'j52vJ99VUyFL5O3owxFkVPsp8',
-  consumer_secret: '9TDSi1nJK6mrIMAdE75vsqSxcS77t0Zt2frVsDcw6tFugDS9SJ',
-  access_token_key: '932566318007533569-LBErrkuzkHgOp5K1Cy1SNyThhshuIWS',
-  access_token_secret: 'ULTBPXCPJbJJ7DlPWqxqzCIC6z2LjpGlMbmzMP0nME7TD',
+var keys = {
+  twitter: {
+    consumer_key: 'j52vJ99VUyFL5O3owxFkVPsp8',
+    consumer_secret: '9TDSi1nJK6mrIMAdE75vsqSxcS77t0Zt2frVsDcw6tFugDS9SJ',
+    access_token_key: '932566318007533569-LBErrkuzkHgOp5K1Cy1SNyThhshuIWS',
+    access_token_secret: 'ULTBPXCPJbJJ7DlPWqxqzCIC6z2LjpGlMbmzMP0nME7TD',
+  },
+  spotify: {
+    id: "c91ca49d03c14e80b4649582f29b6fe3",
+    secret: "1e23df01e1074b5f8d1fca54bf277a6c"
+  },
+  omdb: {
+    apiKey: "40e9cece"
+  }
 }
 
-module.exports = twitterKeys;
+module.exports = keys;
--- liri.js
+++ liri.js
@@ -1,22 +1,75 @@
-var Twitter = require('twitter');
 var fs = require('fs');
-var twitClient = new Twitter(require("./keys.js"));
+var keys = require('./keys.js');
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
+var twitClient = new Twitter(keys.twitter);
 
 function getTweets(strUsername, boolLogOutput) {
-    var params = {screen_name: strUsername, count: 20};
-    twitClient.get('statuses/user_timeline', params, function(error, tweets, response) {
-      if (!error) {
+    var params = { screen_name: strUsername, count: 20 };
+    twitClient.get('statuses/user_timeline', params, function (error, tweets, response) {
         var output = [];
         output.push("============" + new Date() + "=============");
         output.push('Command: get-tweets')
-        output.push('User: ' + tweets[0].user.screen_name)
-        for (i = tweets.length - 1; i >= 0; i--) {
-            output.push("[" + tweets[i].created_at + "] " + tweets[i].text)
+        if (!error) {
+            output.push('User: ' + tweets[0].user.screen_name)
+            for (i = tweets.length - 1; i >= 0; i--) {
+                output.push("[" + tweets[i].created_at + "] " + tweets[i].text)
+            }
+
+        } else {
+            output.push(JSON.stringify(error, null, 2));
         }
         logOutput(output, boolLogOutput);
-      } else {
-          console.error(error);
-      }
+    });
+}
+function spotSong(strSongName, boolLogOutput) {
+    var output = [];
+    output.push("============" + new Date() + "=============");
+    output.push('Command: spotify-this-song')
+    spotify
+    .request('https://api.spotify.com/v1/search?q=track:"'+ strSongName + '"&type=track')
+    .then(function(data) {
+        if(data.tracks.total === 0) {
+            output.push("Song does not exist. Searching for something else instead.");
+            spotify
+            .request('https://api.spotify.com/v1/search?q=track:"The Sign" artist:"Ace of Base"&type=track')
+            .then(function(data) {
+                var output = [];
+                output.push("============" + new Date() + "=============");
+                output.push("============SEARCHING DEFAULT SONG===============");
+                output.push('Command: spotify-this-song')
+                for (var i = 0; i < data.tracks.items.length; i++) {
+                    output.push("Result Number " + (i + 1));
+                    output.push("---------------");
+                    for (var n = 0; n < data.tracks.items[i].artists.length; n++) {
+                        output.push("Artist: " + data.tracks.items[i].artists[n].name);
+                    }
+                    output.push("Album: " + data.tracks.items[i].album.name);
+                    output.push("Track: " + data.tracks.items[i].name);
+                    output.push("Preview: " + data.tracks.items[i].preview_url);
+                }
+                logOutput(output, boolLogOutput);
+            })
+            .catch(function(err) {
+                console.error(err);
+            });
+        } else {
+            for (var i = 0; i < data.tracks.items.length; i++) {
+                output.push("Result Number " + (i + 1));
+                output.push("---------------");
+                for (var n = 0; n < data.tracks.items[i].artists.length; n++) {
+                    output.push("Artist: " + data.tracks.items[i].artists[n].name);
+                }
+                output.push("Album: " + data.tracks.items[i].album.name);
+                output.push("Track: " + data.tracks.items[i].name);
+                output.push("Preview: " + data.tracks.items[i].preview_url);
+            }
+        }
+        logOutput(output, boolLogOutput);
+    }).catch(function(err) {
+        console.error(err);
     });
 }
 
@@ -28,7 +81,7 @@ function logOutput(arrOutput, boolLogOutput) {
     if (boolLogOutput) {
         // console.log("SAVING SHIT");
         for (var i = 0; i < arrOutput.length; i++) {
-            fs.appendFileSync("liriLog.txt", arrOutput[i] + "\r\n", "utf8", function(err) {
+            fs.appendFileSync("liriLog.txt", arrOutput[i] + "\r\n", "utf8", function (err) {
                 if (err) {
                     console.error(err);
                 }
@@ -55,7 +108,7 @@ if (liriCmd === "get-tweets") {
     getTweets(liriCmdArg, liriLogOutput);
 }
 else if (liriCmd === "spotify-this-song") {
-    console.log("Spotify under construction");
+    spotSong(liriCmdArg, liriLogOutput);
 }
 else if (liriCmd === "movie-this") {
     console.log("OMDB under construction");
